78b0c60e708f7f39f7875bdaf2693072a4f38557
Willdta <beyondutraining@gmail.com>
2018-01-29 17:36:13 -0500

    Added working character counter for textarea input

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,1 +1,15 @@
-composer-char-counter.js
+$(function() {
+  $('.new-tweet textarea').keyup(function() {
+    var maxCharacters = 10;
+    var currentCharacters = $(this).val().length;
+    var remaining = maxCharacters - currentCharacters;
+    var character = $(this).siblings('span');
+    character.text(remaining);
+
+    if (currentCharacters > maxCharacters) {
+      character.css('color', 'red');
+    } else {
+      character.css('color', 'black');
+    }
+  });
+});
