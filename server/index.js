756ff7e2bdc9d50f585041b98ec5ab7488281caa
Willdta <beyondutraining@gmail.com>
2018-01-29 16:15:38 -0500

    Added post request for submit button to work

--- server/index.js
+++ server/index.js
@@ -29,6 +29,10 @@ const tweetsRoutes = require("./routes/tweets")(DataHelpers);
 // Mount the tweets routes at the "/tweets" path prefix:
 app.use("/tweets", tweetsRoutes);
 
+app.post('/tweets/', (req, res) => {
+	console.log(res.status());
+});
+
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
 });
