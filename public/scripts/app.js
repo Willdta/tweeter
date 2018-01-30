8d47b4d811c69c626ab8fe080765668e9939a854
Willdta <beyondutraining@gmail.com>
2018-01-29 20:46:20 -0500

    Added code for buttons to appear and disappear on hover

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,3 +4,12 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(function() { 
+  $('#tweet-container').on('mouseenter', function() {
+    $('.icons i').css('opacity', 1);
+  });
+  
+  $('#tweet-container').on('mouseleave', function() {
+    $('.icons i').css('opacity', 0);
+  });
+});
