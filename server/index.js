4724da3cd15395033b0ad1c76804361445e124bf
Willdta <beyondutraining@gmail.com>
2018-02-02 12:30:58 -0500

    Added some comments for clarification of code

--- server/index.js
+++ server/index.js
@@ -14,6 +14,7 @@ app.use(express.static("public"));
 const MongoClient = require("mongodb").MongoClient;
 const MONGODB_URI = "mongodb://localhost:27017/tweeter";
 
+//Connect to Mongo
 MongoClient.connect(MONGODB_URI, (err, db) => {
   if (err) {
     console.error(`Failed to connect: ${MONGODB_URI}`);
